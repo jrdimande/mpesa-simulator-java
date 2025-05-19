6c7508c3123066d1ebd8b3eec3887aa4ef25a802
jrdimande <timoteojrdimande@gmail.com>
2025-05-19 22:23:50 +0000

    feat: criar classe usuario

---
+++ Mpesa/src/Usuario.java
@@ -0,0 +1,53 @@
+public class Usuario {
+    private String nome;
+    private int numero;
+    private double saldo;
+
+    public Usuario(String nome, int numero){
+        this.nome = nome;
+        this.nome = numero;
+        this.saldo = 0;
+    }
+
+    // Metodos especiais
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    public int getNumero(){
+        return numero;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+
+    // Metodos especificos
+
+    public void depositar(int valor){
+        if (valor > 0){
+            this.saldo = this.getSaldo() + valor;
+        }
+    }
+    public void levantar(int valor){
+        if (valor > 0){
+            this.saldo = this.getSaldo() + valor;
+        }
+
+    }
+
+    public String toString(){
+        return "Nome: " + getNome() " Numero: " + getNumero() + "Saldo: " + getSaldo();
+    }
+
+
+}
